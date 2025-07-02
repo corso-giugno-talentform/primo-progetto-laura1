4f6107c975a9f5fd67970a3cf9ac7671954b6e2c
Laura Carbognin <lcarbognin@hotmail.com>
2025-07-02 11:39:10 +0200

    primo commit

---
+++ script1.js
@@ -0,0 +1,54 @@
+
+//DICHIARAZIONI VAR E CONST
+
+
+let eta=32;
+
+const MAX=18;
+
+// CONDIZIONE
+
+
+if (eta>=18)
+{
+console.log("maggiorenne");
+
+    if (eta>=30 && eta<=50)
+    {
+
+        console.log("eta' tra 30 e 50");
+    }
+
+}
+else
+
+{
+console.log("non sei maggiorenne");
+
+}
+
+//operatore ternario
+
+console.log(eta>=MAX ? 'Maggiorenne': 'Minorenne');
+
+// switch case
+
+
+let giorno="lunedi"
+
+switch (giorno)
+{
+
+case "lunedi": console.log("E' lunedi"); break;
+case "martedi": console.log("E' martedi"); break;
+
+}
+
+
+//if valori sempre false, 0,"" null , NaN, undifined
+
+//if valori sempre true tutto il resto
+
+
+
+
